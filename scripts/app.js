01accd05dbb4a3185aa1cd4375fda1efca67991a
Wajdan-Faheem <wajdanf02@gmail.com>
2021-05-23 17:02:00 -0400

    done

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,38 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img')
 
+const updateUI = (data)=>{
+    const cityDets = data.cityDets;
+    const weather = data.weather;
+    
+    //destructing properties
+    //const {cityDets, weather} = data; //does the same thing as above
+    details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+              <div class = 'my-3'>${weather.WeatherText}</div>
+              <div class="display-4 my-4">
+                  <span>${weather.Temperature.Metric.Value}</span>
+                  <span>&deg;C</span>
+              </div>    
+              `;
+    if (weather.isDayTime){
+        time.src = 'icons/day.svg';
+    }
+    else{
+        time.src = 'icons/night.svg '
+    }
+    icon.src=`icons/${weather.WeatherIcon}.svg`;
+    //removing d-none
+    if (card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+}
 const updateCity = async (city)=>{
-    const cityDets = await getCity(city)
-    const weather = await getCondition(cityDets.key);
+    const cityDets = await getCity(city);
+    const weather = await getCondition(cityDets['Key']);
 
     return {
         cityDets:cityDets,
@@ -19,7 +49,7 @@ cityForm.addEventListener('submit',(e)=>{
     //updating city
     updateCity(city).
     then(data =>{
-        console.log(data);
+        updateUI(data)
     })
     .catch(err=>{
         console.log(err.message);
--- scripts/forecaset.js
+++ scripts/forecaset.js
@@ -1,26 +1,28 @@
 const key = "NBPyMehS2Ae36P67B1zsK6EDfRsscs47";
-//get city info
-const getCity = async (city) => {
-    const base =
-        "http://dataservice.accuweather.com/locations/v1/cities/search";
-    const query = `?apikey=${key}&q=${city}`;
-
-    const response = await fetch(base + query);
-    const data = await response.json();
-
-    return data[0];
-};
 
 //get city weather
 getCondition = async (id) => {
-    const base = `http://dataservice.accuweather.com/currentconditions/v1/${id}`;
-    const query = `?apikey=${key}`;
+     const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+     const query = `${id}?apikey=${key}`;
 
     const response = await fetch(base + query);
     const data = await response.json();
 
     return data[0];
 };
+
+
+//get city info
+const getCity = async (city) => {
+ const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
 
 
 
