7413baf4bc340434d04c884c83168a7381e9ac4f
Wajdan-Faheem <wajdanf02@gmail.com>
2021-05-23 13:25:59 -0400

    api call

--- scripts/forecaset.js
+++ scripts/forecaset.js
@@ -1,19 +1,39 @@
-const key = '3VyqKK9wGHz2k8umNkaZ9QTsNnONuckP'; 
-
-const getCity = async (city)=>{
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+const key = "NBPyMehS2Ae36P67B1zsK6EDfRsscs47";
+//get city info
+const getCity = async (city) => {
+    const base =
+        "http://dataservice.accuweather.com/locations/v1/cities/search";
     const query = `?apikey=${key}&q=${city}`;
 
-    const response = await fetch(base+query) ;
+    const response = await fetch(base + query);
     const data = await response.json();
 
     return data[0];
-}
+};
 
-getCity('Manchester') 
-.then((data)=>{
-    console.log(data);
-})
-.catch((err)=>{
-    console.log(err.messsage);
-});
+//get city weather
+getCondition = async (id) => {
+    const base = `http://dataservice.accuweather.com/currentconditions/v1/${id}`;
+    const query = `?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+
+
+
+
+//getCity is called first and then passed through GetCondition with the key which is returend (chaining promises)
+// getCity("Manchester")
+//     .then((data) => {
+//         return getCondition(data["Key"]);
+//     })
+//     .then((data) => {
+//         console.log(data);
+//      })
+//     .catch((err) => {
+//         console.log(err.messsage);
+//     });
